96246e9639335011478dfb9178995a51c37dd928
SaltySalt77 <alexisna@naver.com>
2022-12-08 15:33:00 +0900

    [refact] : union semun 주석처리

--- homework_02/incs/ft_sem.h
+++ homework_02/incs/ft_sem.h
@@ -5,11 +5,13 @@
 
 #define SEM_KEY 5678
 
+/* Mac OS의 sys/sem 헤더파일에는 정의되어있으므로 주석으로 해두었습니다.
 union semun {
     int val;
     struct semid_ds *buf;
     unsigned short *array;
 };
+*/
 
 int create_sem(void);
 void s_signal(int semid);
